8dfa09c9f92eeff2d97288c0d9c48587e51a111b
adaud5 <78923616+adaud5@users.noreply.github.com>
2021-02-11 13:02:24 -0600

    Add files via upload

---
+++ main.C
@@ -0,0 +1,51 @@
+int main()
+{
+	double firstNumb, secondNumb;
+    
+	char operator;
+   
+	for(;;)
+   
+		{
+			printf("Enter function (+, -, *, /, p for power, r for root): ");
+			scanf("%c", &operator);
+
+			printf("Enter two operands: ");
+			scanf("%lf %lf", &firstNumb, &secondNumb);
+			
+			switch(operator)
+			{
+				case '+':
+    	        
+					add(firstNumb, secondNumb);
+    	        
+					break;
+				case '-':
+    	        
+					sub(firstNumb, secondNumb);
+    	        
+					break;
+				case '*':
+    	        
+					mult(firstNumb, secondNumb);
+    	        
+					break;
+				case '/':
+    	        
+					divide(firstNumb, secondNumb);
+    	        
+					break;
+				case 'p':
+    	        
+					power(firstNumb, secondNumb);
+    	        
+					break;
+				case 'r':
+    	        
+					root(firstNumb, secondNumb);
+    	        
+					break;
+			}
+			printf("\n");
+		} 
+}
