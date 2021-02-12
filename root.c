86c00446ec413f4fd73744987b274cfd9ce7a3e1
sukhwindersingh412 <49254880+sukhwindersingh412@users.noreply.github.com>
2021-02-12 11:21:53 -0500

    Update root.c

--- root.c
+++ root.c
@@ -1,7 +1,6 @@
 #include <math.h>
 
 float root(float x, float y){
-    float z = pow(x, 1.0/y);
-    printf("%.2f ^ 1.00/%.2f = %.2f \n", x,y,z);
-    return z; 
+    printf("%.2f ^ 1.00/%.2f = %.2f \n", x,y, pow(x, 1.0/y));
+    return pow(x, 1.0/y); 
 }
